427943a0bbebed66edbfa943833868e6ca8b5a41
fdbiondi <fdbion@gmail.com>
2016-10-15 12:29:14 -0300

    Setters para Jugador

--- src/entidades/Partida.java
+++ src/entidades/Partida.java
@@ -2,60 +2,38 @@ package entidades;
 
 public class Partida {
 
+	//Attributes
 	private int turno;
-
 	private static int puntos_en_disputa;
-	
 	//Player a
-	private Personaje jugador_a;	
+	private Personaje jugador_a; 
 	//Player b
 	private Personaje jugador_b;
 		
-		
+	//Constructor	
 	public Partida(Personaje jugador_a, Personaje jugador_b) {
 		 this.jugador_a = jugador_a;
 		 this.jugador_b = jugador_b;
 		 turno = jugador_a.getId();
 	}
 
-	public int getTurno() {
-		return turno;
-	}
+	//Getters
+	public int getTurno() { return turno; }
 
+	public static int getPuntos_en_disputa() { return puntos_en_disputa; }
 
-	public void setTurno(int turno) {
-		this.turno = turno;
-	}
+	public Personaje getJugador_a() { return jugador_a; }
 
-
-	public static int getPuntos_en_disputa() {
-		return puntos_en_disputa;
-	}
-
+	public Personaje getJugador_b() { return jugador_b; }
+	
+	//Setters
+	public void setTurno(int turno) { this.turno = turno; }
 
 	public static void setPuntos_en_disputa(int puntos_en_disputa) {
 		Partida.puntos_en_disputa = puntos_en_disputa;
 	}
 
+	public void setJugador_a(Personaje jugador_a) { this.jugador_a = jugador_a; }
 
-	public Personaje getJugador_a() {
-		return jugador_a;
-	}
-
-
-	public void setJugador_a(Personaje jugador_a) {
-		this.jugador_a = jugador_a;
-	}
-
-
-	public Personaje getJugador_b() {
-		return jugador_b;
-	}
-
-
-	public void setJugador_b(Personaje jugador_b) {
-		this.jugador_b = jugador_b;
-	}
-	
-	
+	public void setJugador_b(Personaje jugador_b) { this.jugador_b = jugador_b; }
 }
--- src/entidades/Personaje.java
+++ src/entidades/Personaje.java
@@ -2,26 +2,17 @@ package entidades;
 
 public class Personaje {
 	
+	//Attributes
 	private static int counter;
-	
 	private int id;
-	
 	private String nombre;
-	
 	private int vida;
-	
 	private int energia;
-	
 	private int defensa;
-	
 	private int evasion;
-	
 	private int puntosDisponibles;
 	
-	private static int getCounter() {
-		return ++Personaje.counter;
-	}
-
+	//Constructor
 	public Personaje(String nombre) {
 		super();
 		this.id = Personaje.getCounter();
@@ -33,57 +24,54 @@ public class Personaje {
 		this.puntosDisponibles = 200;
 	}
 	
-	public int getId() {
-		return this.id;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public int getVida() {
-		return vida;
-	}
+	//Getters
+	private static int getCounter() { return ++Personaje.counter; }
 	
+	public int getId() { return this.id; }
+	
+	public String getNombre() { return nombre; }
+
+	public int getVida() { return vida; }
+	
+	public int getEnergia() { return energia; }
+	
+	public int getDefensa() { return defensa; }
+
+	public int getEvasion() { return evasion; }
+	
+	public int getPuntosDisponibles() { return puntosDisponibles; }
+
+	//Setters
+	public void setVida(int vida) { this.vida = vida; }
+	
+	public void setEnergia(int energia) { this.energia = energia; }
+	
+	public void setDefensa(int defensa) { this.defensa = defensa; }
+
+	public void setEvasion(int evasion) { this.evasion = evasion; }
+
+	//Methods to add available points to players
 	public void agregarVida(int puntos) {
-		if (this.validaPuntos(puntos, this.vida)) {
-			this.vida = puntos;
-		}
+		if (this.validaPuntos(puntos, this.vida)) 
+			this.vida = puntos;	
 	}
-	
-	public int getEnergia() {
-		return energia;
-	}
-	
+
 	public void agregarEnergia(int puntos) {
-		if (this.validaPuntos(puntos, this.energia)) {
+		if (this.validaPuntos(puntos, this.energia))
 			this.energia = puntos;
-		}
-	}
-	public int getDefensa() {
-		return defensa;
 	}
 	
 	public void agregarDefensa(int puntos) {
-		if (validaPuntos(puntos, this.defensa) && puntos <= 20) {
-			this.defensa = puntos;
-		}
+		if (validaPuntos(puntos, this.defensa) && puntos <= 20) 
+			this.defensa = puntos;	
 	}
 
-	public int getEvasion() {
-		return evasion;
-	}
-	
 	public void agregarEvasion(int puntos) {
-		if (validaPuntos(puntos, this.evasion) && puntos <= 80) {
+		if (validaPuntos(puntos, this.evasion) && puntos <= 80) 
 			this.evasion = puntos;
-		}
 	}
 
-	public int getPuntosDisponibles() {
-		return puntosDisponibles;
-	}
-
+	//Validations
 	private boolean validaPuntos(int total, int atributo) {
 		int puntos = total - atributo;
 		if(puntos <= this.puntosDisponibles){
